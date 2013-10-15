9295f6745378ea0703a6c84369755d48a55686ef
HatchCatch020 <connor1monaghan@gmail.com>
2013-10-15 11:37:17 +0100

    Update gameVariables.java

--- AQADo-Slick/src/sitsa/aqado/gameVariables.java
+++ AQADo-Slick/src/sitsa/aqado/gameVariables.java
@@ -15,5 +15,5 @@ public class gameVariables {
     public static String p2Name;
     public static int fromPause = 0;
 
-    public Box box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11;
+    //public Box box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11;
 }
